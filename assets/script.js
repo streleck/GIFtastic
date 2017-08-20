6f8fb8d9759317801b3f19261a13a793a5d0cfbd
Mark Strelecky <streleck@gmail.com>
2017-08-20 14:57:11 -0500

    changed query address to an https

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ for (i=0; i<topics.length; i++){
 
 
 function gifSearch(word){
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         word + "&limit=10&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
